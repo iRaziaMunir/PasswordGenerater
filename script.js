48df57e29000dd83f0ce631dfb64bbd67a1fa83c
Razia Munir <irazia.munir@gmail.com>
2023-06-21 01:57:48 +0500

    ..

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 let inputSlider = document.getElementById('inputSlider');
 let sliderValue = document.getElementById('sliderValue');
+let passBox = document.getElementById('passBox');
+let lowercase = document.getElementById('lowercase');
+let UpperCase = document.getElementById('UpperCase');
+let numbers = document.getElementById('numbers');
+let symbols = document.getElementById('symbols');
+let genBtn = document.getElementById('genBtn');
 
-sliderValue.textContent = inputSlider.value;
+
+// sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input',()=>{
+    sliderValue.textContent = inputSlider.value;
+})
+
+genBtn.addEventListener('click',()=>{
+    passBox.value = generatePassword();
+})
+
+function generatePassword(){
+    let genPassword = '';
+    genPassword = Math.floor(Math.random()* 10);
+    return genPassword;
+   
+}
