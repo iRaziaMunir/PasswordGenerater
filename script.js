7b9e8647705b2b1d4f8914abe39eab467d7c8188
Razia Munir <irazia.munir@gmail.com>
2023-06-22 22:23:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ let allSymbols = "~!@#$%^&*";
         copyIcon.title = "Password Copied";
 
         setTimeout(() => {
-            copyIcon.innerHTML = "content_copy";
+        copyIcon.querySelector("path").setAttribute("d", "M272 0H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128H192v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z");
+        // copyIcon.innerHTML = "content_copy";
             copyIcon.title = " ";  
         }, 3000);
     }
