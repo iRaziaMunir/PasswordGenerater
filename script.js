21d07af44702154a323e43fe1d9bbefce6e13f8d
Razia Munir <irazia.munir@gmail.com>
2023-06-22 22:25:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -47,12 +47,9 @@ let allSymbols = "~!@#$%^&*";
     if(passBox.value != " " || passBox.value.length >=1) {
         navigator.clipboard.writeText(passBox.value);
         copyIcon.querySelector("path").setAttribute("d", "M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z");
-        copyIcon.title = "Password Copied";
 
         setTimeout(() => {
-        copyIcon.querySelector("path").setAttribute("d", "M272 0H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128H192v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z");
-        // copyIcon.innerHTML = "content_copy";
-            copyIcon.title = " ";  
+            copyIcon.querySelector("path").setAttribute("d", "M272 0H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128H192v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z");
         }, 3000);
     }
 
