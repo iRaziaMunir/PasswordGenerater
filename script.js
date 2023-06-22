27b7136636de2126499cc1c25ca732b206f79798
Razia Munir <irazia.munir@gmail.com>
2023-06-22 13:34:29 +0500

    done

--- script.js
+++ script.js
@@ -8,36 +8,33 @@ let symbols = document.getElementById('symbols');
 let genBtn = document.getElementById('genBtn');
 let copyIcon = document.getElementById('copyIcon');
 
-    sliderValue.textContent = inputSlider.value;
-    inputSlider.addEventListener('input',()=>{
-    sliderValue.textContent = inputSlider.value;
-})
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input',()=>{
+sliderValue.textContent = inputSlider.value;
+});
 
 genBtn.addEventListener('click', ()=>{
-    passBox.value = generatePassword();
-})
+ passBox.value = generatePassword();
+});
+
 let lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
 let UpperCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let allNumbers = "0123456789";
 let allSymbols = "~!@#$%^&*";
 
-
  function generatePassword(){
     let genPassword = " ";
     let allChars = " ";
-    allChars += lowercase.checked ? lowerCharacters : "";
-    allChars += UpperCase.checked ? UpperCharacter : "";
-    allChars += numbers.checked ? allNumbers : "";
-    allChars += symbols.checked ? allSymbols : "";
+        allChars += lowercase.checked ? lowerCharacters : "";
+        allChars += UpperCase.checked ? UpperCharacter : "";
+        allChars += numbers.checked ? allNumbers : "";
+        allChars += symbols.checked ? allSymbols : "";
 
-
-    if (allChars == " "|| allChars.length == 0) {
+    if(allChars == " "|| allChars.length == 0) {
         return genPassword;
     }
-
     let i = 1;
-    while (i<= inputSlider.value) {
-
+    while(i<= inputSlider.value) {
         genPassword += allChars.charAt(Math.floor(Math.random()* allChars.length));
         i++;
     }
@@ -47,16 +44,15 @@ let allSymbols = "~!@#$%^&*";
  }
 
         copyIcon.addEventListener('click' , ()=>{
-    if (passBox.value != " " || passBox.value.length >=1) {
+    if(passBox.value != " " || passBox.value.length >=1) {
         navigator.clipboard.writeText(passBox.value);
-        // copyIcon.innerText = "check";
-        // copyIcon.title = " Password Copied";
+        copyIcon.innerText = "check";
+        copyIcon.title = " Password Copied";
 
-        // setTimeout(() => {
-        //     copyIcon.innerHTML = " content_copy";
-        //     copyIcon.title = " ";
-            
-        // }, 3000);
+        setTimeout(() => {
+            copyIcon.innerHTML = " content_copy";
+            copyIcon.title = " ";  
+        }, 3000);
     }
 
  });
